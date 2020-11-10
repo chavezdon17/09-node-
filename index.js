a8ddd52cb62545d69d28217130885d368ab2c209
Donovan Chavez <chavezdon17@gmail.com>
2020-11-09 20:08:29 -0600

    made inquirer questions

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // array of questions for user
 const questions = [
-
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your location?",
+        name: "location",
+      },
+      {
+        type: "input",
+        message: "what is your LinkedIn Username?",
+        name: "LinkedIn",
+      },
+      {
+        type: "input",
+        message: "what is your Github url?",
+        name: "Github",
+      },
+    ])
+    .then((response) => {
+      const html = generateHTML(response);
+      const fileName = `${response.name.split(" ").join(" ")}.html`;
+      fs.writeFile(fileName, html, (err) => {
+        err ? console.error(err) : console.log("success");
+      });
+    });
+  
 ];
 
 // function to write README file
